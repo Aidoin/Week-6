24c10aaf955f00ff28cd344a9c695a41ada0e378
Aiden Vaitmari <36372544+Aidoin@users.noreply.github.com>
2021-04-11 20:03:48 +0300

    Плановое сохранение 11.04

--- Assets/Scripts/ChickenBehavior.cs
+++ Assets/Scripts/ChickenBehavior.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))] 
+
 public class ChickenBehavior : MonoBehaviour
 {
 
@@ -25,29 +27,21 @@ public class ChickenBehavior : MonoBehaviour
     {
         Transform playerTransform = hub.Player.transform;
 
-        if (playerTransform)
+        Debug.DrawRay(transform.position, (playerTransform.position - transform.position).normalized * visibilityRange_float, Color.blue);
+
+        if (Vector3.Distance(playerTransform.position, transform.position) < visibilityRange_float)
         {
-            Debug.DrawRay(transform.position, (playerTransform.position - transform.position).normalized * visibilityRange_float, Color.blue);
+            Vector3 toPlayer = (playerTransform.position - transform.position).normalized;
+            Vector3 force = rigidbody.mass * (toPlayer * maxSpeed_float - rigidbody.velocity) / timeToMaxSpeed_float;
 
-            if (Vector3.Distance(playerTransform.position, transform.position) < visibilityRange_float)
-            {
-                Vector3 toPlayer = (playerTransform.position - transform.position).normalized;
-                Vector3 force = rigidbody.mass * (toPlayer * maxSpeed_float - rigidbody.velocity) / timeToMaxSpeed_float;
-
-                rigidbody.AddForce(force);
-            }
-            else
-            {
-                if (rigidbody.velocity.magnitude < 0.1f)
-                    rigidbody.velocity = Vector3.zero;
-                else
-                    rigidbody.AddForce(-rigidbody.velocity.normalized * 5f);
-            }
+            rigidbody.AddForce(force);
         }
         else
         {
-            if (FindObjectOfType<PlayerController>())
-                playerTransform = FindObjectOfType<PlayerController>().transform;
+            if (rigidbody.velocity.magnitude < 0.1f)
+                rigidbody.velocity = Vector3.zero;
+            else
+                rigidbody.AddForce(-rigidbody.velocity.normalized * 5f);
         }
     }
 }
---
+++ Assets/Scripts/FixetObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixetObject : MonoBehaviour
+{
+    [SerializeField] private Transform Target;
+
+
+    void Update()
+    {
+        transform.position = Target.position;
+    }
+}
