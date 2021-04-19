5b13c6d85f3eda0d061eaab6fc9606fb574de6fa
Aiden Vaitmari <36372544+Aidoin@users.noreply.github.com>
2021-04-19 22:53:01 +0300

    Плановое сохранение 19.04

--- Assets/Scripts/CameraMoove.cs
+++ Assets/Scripts/CameraMoove.cs
@@ -7,35 +7,129 @@ public class CameraMoove : MonoBehaviour
 
     [SerializeField] private KeyCode zoom_KeyCode;
 
+    [SerializeField] private Vector3 offset = Vector3.zero;
+
     [SerializeField] private float minDistance = 10;
     [SerializeField] private float maxDistance = 20;
 
     private Hub hub;
 
+    private Vector3 sum;
+    private Vector3 target;
+
+    private float distance;
     private bool zoom = true;
 
     private void Awake()
     {
         hub = FindObjectOfType<Hub>();
+
+        distance = minDistance;
+
+        transform.parent = null;
     }
 
 
     void Update()
     {
-        Transform playerTransform = hub.Player.transform;
+        target = hub.Player.transform.position;
+
+        target.y += 1f;
 
         if (Input.GetKeyDown(zoom_KeyCode))
         {
             if (zoom == true)
+            {
                 zoom = false;
+                distance = minDistance;
+            }
             else
+            {
                 zoom = true;
+                distance = maxDistance;
+            }
         }
+        
+
+        sum = hub.Player.transform.position + offset;
+        sum.z += distance;
+
+        //sum = hub.Player.transform.position + offset;
+        //sum = new Vector3(sum.x, sum.y, distance);
+
+        transform.position = Vector3.Lerp(transform.position, sum, Time.deltaTime * 5);
 
 
-        if (zoom)
-            transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, Mathf.MoveTowards(transform.position.z, minDistance, Time.deltaTime * 40));
-        else
-            transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, Mathf.MoveTowards(transform.position.z, maxDistance, Time.deltaTime * 20));
+
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(target - transform.position), Time.deltaTime * 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //    if (Input.GetKeyDown(zoom_KeyCode))
+        //    {
+        //        if (zoom == true)
+        //            zoom = false;
+        //        else
+        //            zoom = true;
+        //    }
+
+
+
+
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(hub.Player.transform.position, Vector3.forward), Time.deltaTime * 10);
+
+        //if (zoom)
+        //    distance = minDistance;
+        //else
+        //    distance = maxDistance;
+
+        //sum = hub.Player.transform.position + offset;
+        //sum.z += distance;
+
+
+
+
+        // transform.position = Vector3.MoveTowards(transform.position, sum, Time.deltaTime * 5);
+
+        //sum = hub.Player.transform.position + offset;
+        //sum = new Vector3(sum.x, sum.y, Mathf.Lerp(sum.z, distance, Time.deltaTime * 10));
+
+        //transform.position = Vector3.MoveTowards(transform.position, new Vector3(result.x, result.y, Mathf.MoveTowards(transform.position.z, minDistance, Time.deltaTime * 40)), Time.deltaTime * 10);
+        //transform.position = Vector3.MoveTowards(transform.position, new Vector3(result.x, result.y, Mathf.MoveTowards(transform.position.z, maxDistance, Time.deltaTime * 20)), Time.deltaTime * 10);
+
     }
 }
